c29e820360ef81b15c69fd0bdf320e757ab5fa59
Mordechai <mordwein77@gmail.com>
2022-10-06 02:12:20 -0400

    commit tree 3ea967b8e76cdf19069b8ba5ab13848f32f74f1c

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+//ask about the && operator and why its working here.
+// also why wouldnt the || operator work.
+function scuberGreetingForFeet(distance) {
+if (distance < 400){return 'This one is on me!'}
+else if (distance > 400 && distance < 2000){return 'That will be twenty bucks.'}
+else if (distance > 2000 && distance < 2500) {return "I will gladly take your thirty bucks."}
+else {return 'No can do.'}
+}
+//ask about the use of == in previous code.
+// how is === working if no type conversions? is city already a string?
+function ternaryCheckCity(city) {
+
+return city === "NYC" ?  'Ok, sounds good.' : "No go."
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+
+  switch (tip) {
+case "generous" : 
+ return "Thank you so much." 
+case "not as generous":
+  return "Thank you."
+default:
+    return "Bye."
+
+  }
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+//Original Code:
+
+// function scuberGreetingForFeet(distance){
+//   if (distance<400){return "This one is on me!"}
+//   if (distance>400 && distance<2000){return 'That will be twenty bucks.'}
+//   if (distance>2500){return "No can do."}
+//   if (distance>2000){return 'I will gladly take your thirty bucks.'}
+
+
+//   }
+
+// function ternaryCheckCity(city){
+//   return city == "NYC" ?  "Ok, sounds good." : "No go."
+// }
+
+
+// function switchOnCharmFromTip(tip){
+//   switch (tip){
+//   case "generous" :
+//     return 'Thank you so much.'
+//     case "not as generous":
+//     return 'Thank you.'
+//     default: 
+//       return 'Bye.'
+    
+//     }
+
